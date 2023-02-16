40aaf3f9301f1d62d675576fe63470982fda8e86
FranBudde <buddemeyerizzo@gmail.com>
2023-02-15 22:40:06 -0300

    login y register terminados

--- app.js
+++ app.js
@@ -10,8 +10,12 @@ app.use (express.static(publicPath));
 app.get("/", (req ,res) => res.sendFile(path.resolve(__dirname,"./views/home.html")));
 
 app.get("/register", (req ,res) => res.sendFile(path.resolve(__dirname,"./views/register.html")));
+app.get("/login", (req ,res) => res.sendFile(path.resolve(__dirname,"./views/login.html")));
 
-app.post("/register", (req,res) => res.redirect("/"))
+
+app.post("/register", (req,res) => res.redirect("/"));
+app.post("/login", (req,res) => res.redirect("/"));
+
 
 
 const port = process.env.PORT || 3030;
